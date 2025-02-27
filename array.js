b1ad71150fe50b30269fba966663eb96e5b8a269
ellis  velandia caicedo <100318892+Ellisvelandia@users.noreply.github.com>
2025-02-27 08:21:15 -0500

    add array manipulation examples and demonstrate spread/rest operators

--- array.js
+++ array.js
@@ -16,4 +16,62 @@ fruits.pop();
 //add to top of the array
 fruits.unshift("Lemon");
 
-console.log(fruits);
+//Remove from top of the array
+fruits.shift();
+
+// console.log(fruits);
+
+//loop array
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+
+// let i = 0;
+// while (i < fruits.length) {
+//   console.log(fruits[i]);
+//   i++;
+// }
+
+// inbuilt loop methods
+// const numbers = [1, 2, 3, 4, 5];
+
+// const newNumbers = numbers.map((item, index, array) => {
+//   return item * 2;
+// });
+
+// const newNumbers = numbers.filter((item, index, array) => {
+//   return item > 3;
+// })
+
+// const newNumbers = numbers.reduce((prev, item) => {
+//   return prev + item;
+// }, 0);
+
+// const newNumbers = numbers.some((item, index, array) => {
+//   return item > 4;
+// })
+
+// const newNumbers = numbers.every((item, index, array) => {
+//   return item > 4;
+// })
+
+// const newNumbers = numbers.find((item, index, array) => { //find the first item that matches the condition
+//   return item > 2;
+// })
+
+// console.log(newNumbers);
+
+//Spread Operator and Rest Operator
+
+const nums = [1, 2, 3, 4, 5];
+const nums2 = [6, 7, 8, 9, 10];
+
+// const finalNums = [...nums, ...nums2]; //spread operator
+
+// console.log(finalNums);
+
+function sum(...numbers) {
+  return numbers;
+}
+
+console.log(sum(nums, nums2, "ellis"));
